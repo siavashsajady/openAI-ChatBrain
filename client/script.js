45e5b4abd3e2ad0f5a56f7a7e83f1295bc713b49
Siavash Sajady <hubgitpro@gmail.com>
2022-12-30 12:01:54 +0000

    Added typeText function to project

--- client/script.js
+++ client/script.js
@@ -17,3 +17,20 @@ function loader(element) {
     }
   }, 300);
 }
+
+//Implement typing functionality
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    //if user typing
+    if (index < text.length) {
+      //get the character under specific index in the text that AI return
+      element.innerHTML += text.chartAt(index);
+      index++;
+    } else {
+      // if we reached the end of the text
+      clearInterval(interval);
+    }
+  }, 20);
+}
