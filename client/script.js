8d69496853edf726dd4a13aaaec264073a12b50d
Siavash Sajady <hubgitpro@gmail.com>
2022-12-30 17:36:48 +0000

    Updated the server url

--- client/script.js
+++ client/script.js
@@ -90,7 +90,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch the data from the server -> get bot's response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://chatbrainapp.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
